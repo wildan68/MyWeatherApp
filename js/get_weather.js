fedc6993c5eaee8605a4ad880bb18a458f80b5cd
wildan68 <outsiderkemuning@gmail.com>
2021-11-29 23:46:29 +0700

    update

--- js/get_weather.js
+++ js/get_weather.js
@@ -1,16 +1,17 @@
 var root = new Vue({
     el: '#app',
-    data() {
-        return {
-            info_main: null,
-            info_sys: null,
-            info_weather: null,
-            info: null,
-            loading: true,
-            errored: false,
-            searchCity: '',
-            default_city: 'dubai',
-        }
+    data: {
+        info_main: null,
+        info_sys: null,
+        info_weather: null,
+        info: null,
+        loading: true,
+        errored: false,
+        searchCity: '',
+        default_city: true,
+        ip: null,
+        ip_data: null,
+        ip_me: '',
     },
     methods: {
         getWeather(city) {
@@ -41,19 +42,28 @@ var root = new Vue({
             this.getWeather(this.searchCity)
         }
     },
-    created() {
+    mounted() {
         axios
-            .get('https://api.openweathermap.org/data/2.5/weather?q=' + this.default_city + '&lang=id&appid=ea8535a0ff11a4e50f1a25f6cedfa299')
+            .get('https://api.ipify.org/?format=json')
             .then(response => {
-                this.info_main = response.data.main
-                this.info_sys = response.data.sys
-                this.info_weather = response.data.weather
-                this.info = response.data
+                this.ip = response.data,
+                    this.ip_me = this.ip.ip
             })
             .catch(error => {
                 console.log(error)
                 this.errored = true
             })
             .finally(() => this.loading = false)
-    }
+        axios
+            .get('http://www.geoplugin.net/json.gp?ip=' + this.ip_me)
+            .then(response => {
+                this.ip_data = response.data,
+                    this.getWeather(this.ip_data.geoplugin_city)
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => this.loading = false)
+    },
 })
